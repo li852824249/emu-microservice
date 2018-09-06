472dd131b3d5194b0f1bac6831b7d2435f0d851d
lijinzhou <lijinzhou@httc.com.cn>
2018-09-06 17:15:10 +0800

    注解@RequestBody 添加

--- emu-microservice-consumer-dept-80/src/main/java/com/cn/emu/controller/DeptController.java
+++ emu-microservice-consumer-dept-80/src/main/java/com/cn/emu/controller/DeptController.java
@@ -14,24 +14,25 @@ import com.cn.emu.entities.Dept;
 @RestController
 public class DeptController {
 	
-	private static final String REST_URLPREFIX = "http://localhost:8001";
+	private static final String REST_URL_PREFIX = "http://localhost:8001";
 	
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@RequestMapping(value="/consumer/dept/add",method=RequestMethod.POST)
-	public boolean add(Dept dept) {
-		return restTemplate.postForObject(REST_URLPREFIX+"/dept/add", dept, Boolean.class);
-	}
+	@RequestMapping(value="/consumer/dept/add")
+    public boolean add(Dept dept) {
+         return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add", dept, Boolean.class);
+    }
+
 	
 	@RequestMapping(value="/consumer/dept/get/{id}",method=RequestMethod.GET)
 	public Dept get(@PathVariable("id") Long id) {
-		return restTemplate.getForObject(REST_URLPREFIX+"/dept/get/"+id, Dept.class);
+		return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id, Dept.class);
 	}
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value="/consumer/dept/list",method=RequestMethod.GET)
 	public List<Dept> list(){
-		return restTemplate.getForObject(REST_URLPREFIX+"/dept/list", List.class);
+		return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list", List.class);
 	}
 }
--- emu-microservice-provider-dept-8001/src/main/java/com/cn/emu/controller/DeptController.java
+++ emu-microservice-provider-dept-8001/src/main/java/com/cn/emu/controller/DeptController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class DeptController {
 	private DeptService deptService;
 	
 	@RequestMapping(value="/dept/add",method=RequestMethod.POST)
-	public boolean add(Dept dept){
+	public boolean add(@RequestBody Dept dept){
 		return deptService.add(dept);
 	}
 	
